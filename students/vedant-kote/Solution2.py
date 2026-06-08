972ca6f1fa07f6d3d1b220cbf72670b1bd8367c7
kotevedant8-gif <kotevedant8@gmail.com>
2026-06-08 10:36:19 +0530

    Update Solution2.py

--- students/vedant-kote/Solution2.py
+++ students/vedant-kote/Solution2.py
@@ -6,3 +6,20 @@ print(d["Vijay"])
 d.update({"Tom":2,"Don": 10})
 print(d)
 
+#problem no3
+s = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",  "k", "l", "m", "n", "o", "p", "q", "r", "s",  "t", "u", "v", "w", "x", "y", "z"}
+
+v = 0
+c= 0
+
+vowels = "aeiou"
+
+for x in s:
+    if x in vowels:
+        v = v + 1  
+    else:
+        c = c+ 1  
+
+print("Total Vowels:", v)
+print("Total Consonants:", c)
+
