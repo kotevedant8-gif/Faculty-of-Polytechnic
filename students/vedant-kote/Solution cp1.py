5f452d9276ee3fc62e05918135254d177bd3bd0a
kotevedant8-gif <kotevedant8@gmail.com>
2026-06-17 10:10:09 +0530

    Create Solution cp1.py

---
+++ students/vedant-kote/Solution cp1.py
@@ -0,0 +1,23 @@
+#avg
+def avrage_of_three(m1,m2,m3):
+    return round(m1,m2,m3)/3
+#result  
+def classify_result(a):
+    if a>=75:
+        return("distinction")
+    elif a>=35:
+        return("pass")
+    else:
+        return("fall")
+ #grade
+  def classify_result(a):
+    if a>=90:
+        return("A")
+    elif a>=80:
+        return("B")
+    elif a>=70:
+        return("C")
+    elif a>=60:
+        return("d")
+    elif a<60:
+        return("F")
