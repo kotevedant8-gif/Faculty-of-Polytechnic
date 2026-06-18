cfdfa3e787915385e7274bc0a211385a10f2a9a2
kotevedant8-gif <kotevedant8@gmail.com>
2026-06-18 09:18:09 +0530

    Create Cp2.py

---
+++ students/vedant-kote/Cp2.py
@@ -0,0 +1,40 @@
+#problem1
+def ask_text():
+    while True:
+        name = input("What is your name? ")
+        if name == "":
+            return("Can't be empty. Try again.")
+        else:
+            return(name)
+            break
+    while True:
+        sub_name = input("Enter subject name? ")
+        if sub_name == "":
+            return("Can't be empty. Try again.")
+        else:
+            return(sub_name)
+            break
+
+  #problem2
+  def ask_marks(): 
+    while True: 
+        user_input = input("Enter your marks 1-100: ") 
+        if user_input.isdigit(): 
+            return(user_input)
+            break
+        else: 
+            return("Enter whole number only")
+ask_marks() 
+#problem3
+ def ask_y_n():
+    while True:
+        user_input=input("do you want to add another subject:-")
+        if user_input == "y":
+            return(True)
+            break
+        elif user_input == "n":
+            return(False)
+            break
+        else:
+            print("type only y/n only")
+ask_y_n()
